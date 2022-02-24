830c25c7f40aeb9ee3360828a9c4210170455667
Nathaniel Nkrumah <nightingale.nath2@gmail.com>
2022-02-24 00:43:35 +0000

    Update database.php

--- database.php
+++ database.php
@@ -4,7 +4,7 @@ public $db;
 public function getConnection(){
 $this->db = null;
 try{
-$this->db = new mysqli('localhost','root','','apicruddb');
+$this->db = new mysqli('remotemysql.com','Jc8lsyo13T','xnCjtmp6L2','Jc8lsyo13T');
 }catch(Exception $e){
 echo "Database could not be connected: " . $e->getMessage();
 }
